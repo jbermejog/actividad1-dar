35a2851fdede2b2058cdfe0c3c99043fb4bf5578
Javier <javier.bermejo@me.com>
2024-12-28 12:54:05 +0100

    cambio de ruta

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ const AppContent = () => {
     <div className="App">
       <Navbar />
       {/* Renderiza Sidebar o SidebarMedium según la ruta */}
-      {location.pathname.startsWith('/actividad1-dar/tutorial-basico') && <Sidebar />}
-      {location.pathname.startsWith('/actividad1-dar/tutorial-nivel-medio') && <SidebarMedium />}
+      {location.pathname.startsWith('/actividad1-dar/tutorial-basico/') && <Sidebar />}
+      {location.pathname.startsWith('/actividad1-dar/tutorial-nivel-medio/') && <SidebarMedium />}
       {/* Puedes agregar más sidebars si tienes más rutas */}
       <div className="main-content">
         <Routes>
-          <Route path="/" element={<Navigate to="/actividad1-dar/tutorial-basico" />} />
-          <Route path="/tutorial-basico" element={<Navigate to="/actividad1-dar/tutorial-basico" />} />
+          <Route path="/" element={<Navigate to="/actividad1-dar/tutorial-basico/" />} />
+          {/* <Route path="/tutorial-basico" element={<Navigate to="/actividad1-dar/tutorial-basico" />} /> */}
           <Route path="/actividad1-dar/tutorial-basico/*" element={<MainContent />} />
           <Route path="/actividad1-dar/tutorial-nivel-medio/*" element={<MediumContent />} />
           {/* Nueva Ruta para Referencias */}
